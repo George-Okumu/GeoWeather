4c56d655192f049f9bbc274da245efcb31f3d873
George Okumu <gokumu12@gmail.com>
2020-10-22 22:41:05 +0300

    Implemented list View and array adapter

--- app/src/main/java/com/moringa/geoweather/WeatherActivity.java
+++ app/src/main/java/com/moringa/geoweather/WeatherActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WeatherActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+    String[] weatherList = new String[]  {"Rains over a few places",
+            "Showers and thunderstorms over several places. ",
+            "Showers over a few places", "Showers over a few  places", "25-35oC "};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +27,21 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weather);
         ButterKnife.bind(this);
 
+        ArrayAdapter adapter = new ArrayAdapter(WeatherActivity.this, android.R.layout.simple_list_item_1, weatherList);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String weatherList = ((TextView)view).getText().toString();
+                Toast.makeText(WeatherActivity.this, weatherList, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-
         mLocationTextView.setText("This is the today,s weather forecast in: " + location);
+
+
     }
 }
