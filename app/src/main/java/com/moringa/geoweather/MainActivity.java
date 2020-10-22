35f9992233301c84a57f423f1ef11c5c1b00aac3
George Okumu <gokumu12@gmail.com>
2020-10-22 21:33:22 +0300

    Introduced another Activity

--- app/src/main/java/com/moringa/geoweather/MainActivity.java
+++ app/src/main/java/com/moringa/geoweather/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringa.geoweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                startActivity(intent);
                 Toast.makeText(MainActivity.this, "Welcome to Weather App", Toast.LENGTH_LONG).show();
             }
         });
