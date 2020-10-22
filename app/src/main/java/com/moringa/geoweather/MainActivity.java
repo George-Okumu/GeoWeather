ca78ff2d2b9b0e18994177be9607fab1375741c9
George Okumu <gokumu12@gmail.com>
2020-10-22 21:30:25 +0300

    Introduced a click listener and a toast,...

--- app/src/main/java/com/moringa/geoweather/MainActivity.java
+++ app/src/main/java/com/moringa/geoweather/MainActivity.java
@@ -3,13 +3,27 @@ package com.moringa.geoweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.searchWeatherButton) Button mSearchWeatherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mSearchWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome to Weather App", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
