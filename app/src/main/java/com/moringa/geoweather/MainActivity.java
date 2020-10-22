e7e5aff637984bb411aded984e8bad8c97c4ae1f
George Okumu <gokumu12@gmail.com>
2020-10-22 22:21:07 +0300

    Gathered data using string extra

--- app/src/main/java/com/moringa/geoweather/MainActivity.java
+++ app/src/main/java/com/moringa/geoweather/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.searchWeatherButton) Button mSearchWeatherButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
         mSearchWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, "onClick: "+ location);
                 Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
                 Toast.makeText(MainActivity.this, "Welcome to Weather App", Toast.LENGTH_LONG).show();
             }
--- app/src/main/java/com/moringa/geoweather/WeatherActivity.java
+++ app/src/main/java/com/moringa/geoweather/WeatherActivity.java
@@ -2,13 +2,25 @@ package com.moringa.geoweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WeatherActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mLocationTextView.setText("This is the today,s weather forecast in: " + location);
     }
 }
