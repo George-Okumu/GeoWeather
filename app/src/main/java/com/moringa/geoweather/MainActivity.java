2fda0a4416a35027b908326bd01edc46a04d3a9c
George Okumu <gokumu12@gmail.com>
2020-10-22 23:11:22 +0300

    Implemented a onClickListener Interface

--- app/src/main/java/com/moringa/geoweather/MainActivity.java
+++ app/src/main/java/com/moringa/geoweather/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.searchWeatherButton) Button mSearchWeatherButton;
     @BindView(R.id.locationEditText) EditText mLocationEditText;
@@ -24,16 +24,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        mSearchWeatherButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Log.d(TAG, "onClick: "+ location);
-                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, "Welcome to Weather App", Toast.LENGTH_LONG).show();
-            }
-        });
+        mSearchWeatherButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSearchWeatherButton){
+
+            String location = mLocationEditText.getText().toString();
+            Log.d(TAG, "onClick: "+ location);
+            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "Welcome to Weather App", Toast.LENGTH_LONG).show();
+        }
     }
 }
